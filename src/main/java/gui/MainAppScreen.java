deb36d7065e8cd9fa701b6da7f5bcb5200e8ec0e
Mattheo de Wit <M.C.A.deWit@student.tudelft.nl>
2021-08-05 14:15:33 +0200

    First project structure basis.

---
+++ src/main/java/gui/MainAppScreen.java
@@ -0,0 +1,13 @@
+package gui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainAppScreen extends Application {
+    private static Stage stage;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
