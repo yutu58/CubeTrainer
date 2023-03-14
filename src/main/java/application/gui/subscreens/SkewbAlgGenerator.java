2005561c22a9484e4bfd729da53ecb589ed94b0d
Mattheo de Wit <mattheodw@gmail.com>
2023-03-14 21:58:51 +0100

    Skewb alg generator (#2)
    * Add alg generator and some other changes
    
    * Catch runtime exceptions and remove printlns
    
    * Finish up alg generator, ready for test version 0.1.0.
    
    * Add copyright info

--- src/main/java/application/gui/subscreens/SkewbAlgGenerator.java
+++ src/main/java/application/gui/subscreens/SkewbAlgGenerator.java
@@ -3,33 +3,24 @@ package application.gui.subscreens;
 import application.controllers.SkewbScreenController;
 import cubes.skewb.SkewbNotations;
 import cubes.skewb.SkewbState;
-import cubes.skewb.imageGenerators.SkewbL2LImageGenerator;
-import cubes.skewb.solvers.SkewbScrambler;
 import cubes.skewb.solvers.SkewbSolver;
-import javafx.embed.swing.SwingFXUtils;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
-import javafx.scene.canvas.Canvas;
+import javafx.scene.Node;
 import javafx.scene.control.*;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.image.WritableImage;
 import javafx.scene.input.Clipboard;
 import javafx.scene.input.ClipboardContent;
 import javafx.scene.input.DataFormat;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 
-import javax.imageio.ImageIO;
-import java.io.File;
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -44,7 +35,16 @@ public class SkewbAlgGenerator extends GridPane implements Initializable {
     private Label imageErrorLabel;
 
     @FXML
-    private Canvas imageCanvas;
+    private TextField setupMoves;
+
+    @FXML
+    private Slider depthSlider;
+
+    @FXML
+    private CheckBox reverseBox;
+
+    @FXML
+    private CheckBox allAngles;
 
     @FXML
     private RadioButton rubikSkewbButton;
@@ -56,27 +56,13 @@ public class SkewbAlgGenerator extends GridPane implements Initializable {
     private RadioButton codeSkewbButton;
 
     @FXML
-    private Label copyImageButton;
+    private ListView<String> algList;
 
     @FXML
-    private Label downloadPNGButton;
+    private HBox rotationExplorer;
 
     @FXML
-    private Label copyLithiumCodeButton;
-
-    @FXML
-    private TextField setupMoves;
-
-    @FXML
-    private Slider scaleSlider;
-
-    @FXML
-    private CheckBox reverseBox;
-
-    @FXML
-    private CheckBox bottomBox;
-
-    private String pattern;
+    private Label copyAlgButton;
 
     private SkewbScreenController controller;
 
@@ -98,16 +84,117 @@ public class SkewbAlgGenerator extends GridPane implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         applyButton.setOnAction((e) -> go());
+
+        notationGroup = new ToggleGroup();
+
+        rubikSkewbButton.setToggleGroup(notationGroup);
+        rubikSkewbButton.setSelected(true);
+        wcaSkewbButton.setToggleGroup(notationGroup);
+        codeSkewbButton.setToggleGroup(notationGroup);
+
+        algList.setOnMousePressed(event -> {
+            Optional<String> optional = algList.getSelectionModel().getSelectedItems().stream().findFirst();
+            if (optional.isEmpty()) return;
+            setHighlightedAlg(optional.get());
+        });
+
+        algList.setOnKeyPressed(event -> {
+            if (event.getCode().isArrowKey()){
+                Optional<String> optional = algList.getSelectionModel().getSelectedItems().stream().findFirst();
+                if (optional.isEmpty()) return;
+                setHighlightedAlg(optional.get());
+            }
+        });
+
+        Label l = copyAlgButton;
+        l.setBorder(new Border(new BorderStroke(MENU_COLOR, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(2))));
+        l.setOnMouseEntered((e) -> l.setBackground(new Background(new BackgroundFill(SUBMENU_COLOR, CornerRadii.EMPTY, Insets.EMPTY))));
+        l.setOnMousePressed((e) -> l.setBackground(new Background(new BackgroundFill(SELECTED_SUBMENU_BUTTON_COLOR, CornerRadii.EMPTY, Insets.EMPTY))));
+        l.setOnMouseReleased((e) -> l.setBackground(new Background(new BackgroundFill(SUBMENU_COLOR, CornerRadii.EMPTY, Insets.EMPTY))));
+        l.setOnMouseExited((e) -> l.setBackground(null));
+
+        l.setOnMouseClicked((e) -> copyAlgToClipboard());
+        l.setVisible(false);
+    }
+
+    private void copyAlgToClipboard() {
+        Clipboard clipboard = Clipboard.getSystemClipboard();
+        ClipboardContent content = new ClipboardContent();
+        StringBuilder finalAlg = new StringBuilder();
+        for (Node child : rotationExplorer.getChildren()) {
+            Label l = (Label) child;
+            if (l.getText().equals(" - ")) {
+                finalAlg.append(" ");
+            } else {
+                finalAlg.append(l.getText());
+            }
+        }
+        content.put(DataFormat.PLAIN_TEXT, finalAlg.toString());
+        clipboard.setContent(content);
+        promptSuccess("Alg copied to clipboard!");
+    }
+
+    private void setHighlightedAlg(String alg) {
+        copyAlgButton.setVisible(true);
+        ObservableList<Node> children = rotationExplorer.getChildren();
+        children.remove(0, children.size());
+
+        String[] moves = alg.split(" ");
+        //Create z select element
+        for (int i = 0; i < moves.length; i++) {
+            Label x = new Label(moves[i]);
+            x.setFont(new Font(30));
+            x.setStyle("-fx-font-weight: Bold");
+            children.add(x);
+
+            if (i != moves.length - 1) {
+                Label y = new Label(" - ");
+                int finalI = i;
+                y.setFont(new Font(30));
+                children.add(y);
+                y.setOnMouseClicked(event -> {
+                    ObservableList<Node> childrenLeft = rotationExplorer.getChildren();
+                    //Update moves after
+                    for (int j = (finalI + 1) * 2; j < childrenLeft.size(); j += 2) {
+                        Label l = (Label) childrenLeft.get(j);
+                        l.setText(SkewbNotations.nextInZ(l.getText()));
+
+                    }
+                    //Update rotation move itself
+                    y.setText(SkewbNotations.nextInZ(y.getText()));
+                });
+
+                y.setOnMouseEntered(event -> y.setStyle("-fx-underline: true; -fx-text-fill: blue; -fx-cursor: hand"));
+                y.setOnMouseExited(event -> y.setStyle("-fx-underline: false; -fx-text-fill: black; -fx-cursor: default"));
+            }
+        }
     }
 
     private void go() {
-        pattern = SkewbL2LImageGenerator.drawImageFromSetup(SkewbNotations.notationEnum.RubikSkewbNotation,
-                setupMoves.getText(), imageCanvas.getGraphicsContext2D(), 1, false);
-        SkewbState s = new SkewbState(pattern);
-        SkewbSolver solver = new SkewbSolver(s, 15, imageErrorLabel);
+        RadioButton selected = (RadioButton) notationGroup.getSelectedToggle();
+        SkewbNotations.notationEnum n;
 
-        ExecutorService es = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
-        es.submit(solver);
+        if (selected == rubikSkewbButton) {
+            n = SkewbNotations.notationEnum.RubikSkewbNotation;
+        } else if (selected == wcaSkewbButton) {
+            n = SkewbNotations.notationEnum.WCASkewbNotation;
+        } else if (selected == codeSkewbButton) {
+            n = SkewbNotations.notationEnum.LithiumSkewbCode;
+        } else {
+            promptError("Select a valid input notation");
+            return;
+        }
+
+        try {
+            SkewbState s = SkewbState.setupCase(setupMoves.getText(), n, reverseBox.isSelected());
+
+            SkewbSolver solver = new SkewbSolver(s, (int) depthSlider.getValue(), imageErrorLabel, algList, allAngles.isSelected());
+
+            ExecutorService es = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+            es.submit(solver);
+        } catch (RuntimeException e){
+            promptError("Please provide a valid input!");
+        }
     }
 
     private void promptSuccess(String succ) {
